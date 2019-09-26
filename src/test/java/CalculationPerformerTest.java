e771a35cc7ab7472394d5a189bac53df8a899223
ater <alexandr.ter@endava.com>
2019-09-26 17:02:53 +0300

    @ExtendWith

--- src/test/java/CalculationPerformerTest.java
+++ src/test/java/CalculationPerformerTest.java
@@ -1,21 +1,27 @@
 import java.awt.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Matchers.anyDouble;
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class CalculationPerformerTest {
 
-    private Calculator calculator = mock(Calculator.class);
+    @Mock
+    private Calculator calculator;
 
-    private CalculationPerformer calculationPerformer = new CalculationPerformer(calculator);
+    @InjectMocks
+    private CalculationPerformer calculationPerformer;
 
     @Test
     public void testPerformMultiplicationEven() {
