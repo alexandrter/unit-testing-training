58b9eadad05582a62ca871e1e03899e29b8c25ea
ater <alexandr.ter@endava.com>
2019-09-26 16:40:06 +0300

    CalculationPerformer test refactoring

--- src/main/java/CalculationResult.java
+++ src/main/java/CalculationResult.java
@@ -1,9 +1,20 @@
 import java.awt.*;
+import java.util.Objects;
 
 public class CalculationResult {
 
     private double number;
     private Color color;
+
+    //noargs constructor
+    public CalculationResult() {
+
+    }
+    //allargs constructor
+    public CalculationResult(final double number, final Color color) {
+        this.number = number;
+        this.color = color;
+    }
 
     //getters & setters
     public double getNumber() {
@@ -20,5 +31,23 @@ public class CalculationResult {
 
     public void setColor(final Color color) {
         this.color = color;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final CalculationResult that = (CalculationResult) o;
+        return Double.compare(that.number, number) == 0 &&
+                Objects.equals(color, that.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number, color);
     }
 }
--- src/test/java/CalculationPerformerTest.java
+++ src/test/java/CalculationPerformerTest.java
@@ -1,11 +1,15 @@
+import java.awt.*;
+
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Matchers.anyDouble;
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.only;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class CalculationPerformerTest {
 
@@ -14,30 +18,138 @@ public class CalculationPerformerTest {
     private CalculationPerformer calculationPerformer = new CalculationPerformer(calculator);
 
     @Test
-    public void testPerformMultiplication() {
-        calculationPerformer.getCalculationResult(10, 5, Operator.MULTIPLICATION);
+    public void testPerformMultiplicationEven() {
+        when(calculator.multiply(anyDouble(), anyDouble())).thenReturn(50d);
+        when(calculator.isOdd(anyInt())).thenReturn(false);
+
+        final CalculationResult actualCalculationResult = calculationPerformer.getCalculationResult(10, 5, Operator.MULTIPLICATION);
 
         verify(calculator, times(1)).multiply(10, 5);
+        verify(calculator, times(1)).isOdd(50);
         verify(calculator, never()).divide(anyDouble(), anyDouble());
         verify(calculator, never()).add(anyDouble(), anyDouble());
         verify(calculator, never()).subtract(anyDouble(), anyDouble());
+
+        final CalculationResult expectedCalculationResult = new CalculationResult(50, Color.GRAY);
+        assertEquals(expectedCalculationResult, actualCalculationResult);
     }
 
     @Test
-    public void testPerformDivision() {
-        calculationPerformer.getCalculationResult(10, 5, Operator.DIVISION);
-        verify(calculator, only()).divide(10, 5);
+    public void testPerformMultiplicationOdd() {
+        when(calculator.multiply(anyDouble(), anyDouble())).thenReturn(51d);
+        when(calculator.isOdd(anyInt())).thenReturn(true);
+
+        final CalculationResult actualCalculationResult = calculationPerformer.getCalculationResult(10, 5, Operator.MULTIPLICATION);
+
+        verify(calculator, times(1)).multiply(10, 5);
+        verify(calculator, times(1)).isOdd(51);
+        verify(calculator, never()).divide(anyDouble(), anyDouble());
+        verify(calculator, never()).add(anyDouble(), anyDouble());
+        verify(calculator, never()).subtract(anyDouble(), anyDouble());
+
+        final CalculationResult expectedCalculationResult = new CalculationResult(51, Color.RED);
+        assertEquals(expectedCalculationResult, actualCalculationResult);
     }
 
     @Test
-    public void testPerformAddition() {
-        calculationPerformer.getCalculationResult(10, 5, Operator.ADDITION);
-        verify(calculator, only()).add(10, 5);
+    public void testPerformDivisionEven() {
+        when(calculator.divide(anyDouble(), anyDouble())).thenReturn(50d);
+        when(calculator.isOdd(anyInt())).thenReturn(false);
+
+        final CalculationResult actualCalculationResult = calculationPerformer.getCalculationResult(10, 5, Operator.DIVISION);
+
+        verify(calculator, times(1)).divide(10, 5);
+        verify(calculator, times(1)).isOdd(50);
+        verify(calculator, never()).multiply(anyDouble(), anyDouble());
+        verify(calculator, never()).add(anyDouble(), anyDouble());
+        verify(calculator, never()).subtract(anyDouble(), anyDouble());
+
+        final CalculationResult expectedCalculationResult = new CalculationResult(50, Color.GRAY);
+        assertEquals(expectedCalculationResult, actualCalculationResult);
     }
 
     @Test
-    public void testPerformSubtraction() {
-        calculationPerformer.getCalculationResult(10, 5, Operator.SUBTRACTION);
-        verify(calculator, only()).subtract(10, 5);
+    public void testPerformDivisionOdd() {
+        when(calculator.divide(anyDouble(), anyDouble())).thenReturn(51d);
+        when(calculator.isOdd(anyInt())).thenReturn(true);
+
+        final CalculationResult actualCalculationResult = calculationPerformer.getCalculationResult(10, 5, Operator.DIVISION);
+
+        verify(calculator, times(1)).divide(10, 5);
+        verify(calculator, times(1)).isOdd(51);
+        verify(calculator, never()).multiply(anyDouble(), anyDouble());
+        verify(calculator, never()).add(anyDouble(), anyDouble());
+        verify(calculator, never()).subtract(anyDouble(), anyDouble());
+
+        final CalculationResult expectedCalculationResult = new CalculationResult(51, Color.RED);
+        assertEquals(expectedCalculationResult, actualCalculationResult);
+    }
+
+    @Test
+    public void testPerformAdditionEven() {
+        when(calculator.add(anyDouble(), anyDouble())).thenReturn(50d);
+        when(calculator.isOdd(anyInt())).thenReturn(false);
+
+        final CalculationResult actualCalculationResult = calculationPerformer.getCalculationResult(10, 5, Operator.ADDITION);
+
+        verify(calculator, times(1)).add(10, 5);
+        verify(calculator, times(1)).isOdd(50);
+        verify(calculator, never()).divide(anyDouble(), anyDouble());
+        verify(calculator, never()).multiply(anyDouble(), anyDouble());
+        verify(calculator, never()).subtract(anyDouble(), anyDouble());
+
+        final CalculationResult expectedCalculationResult = new CalculationResult(50, Color.GRAY);
+        assertEquals(expectedCalculationResult, actualCalculationResult);
+    }
+
+    @Test
+    public void testPerformAdditionOdd() {
+        when(calculator.add(anyDouble(), anyDouble())).thenReturn(51d);
+        when(calculator.isOdd(anyInt())).thenReturn(true);
+
+        final CalculationResult actualCalculationResult = calculationPerformer.getCalculationResult(10, 5, Operator.ADDITION);
+
+        verify(calculator, times(1)).add(10, 5);
+        verify(calculator, times(1)).isOdd(51);
+        verify(calculator, never()).divide(anyDouble(), anyDouble());
+        verify(calculator, never()).multiply(anyDouble(), anyDouble());
+        verify(calculator, never()).subtract(anyDouble(), anyDouble());
+
+        final CalculationResult expectedCalculationResult = new CalculationResult(51, Color.RED);
+        assertEquals(expectedCalculationResult, actualCalculationResult);
+    }
+
+    @Test
+    public void testPerformSubtractionEven() {
+        when(calculator.subtract(anyDouble(), anyDouble())).thenReturn(50d);
+        when(calculator.isOdd(anyInt())).thenReturn(false);
+
+        final CalculationResult actualCalculationResult = calculationPerformer.getCalculationResult(10, 5, Operator.SUBTRACTION);
+
+        verify(calculator, times(1)).subtract(10, 5);
+        verify(calculator, times(1)).isOdd(50);
+        verify(calculator, never()).divide(anyDouble(), anyDouble());
+        verify(calculator, never()).multiply(anyDouble(), anyDouble());
+        verify(calculator, never()).add(anyDouble(), anyDouble());
+
+        final CalculationResult expectedCalculationResult = new CalculationResult(50, Color.GRAY);
+        assertEquals(expectedCalculationResult, actualCalculationResult);
+    }
+
+    @Test
+    public void testPerformSubtractionOdd() {
+        when(calculator.subtract(anyDouble(), anyDouble())).thenReturn(51d);
+        when(calculator.isOdd(anyInt())).thenReturn(true);
+
+        final CalculationResult actualCalculationResult = calculationPerformer.getCalculationResult(10, 5, Operator.SUBTRACTION);
+
+        verify(calculator, times(1)).subtract(10, 5);
+        verify(calculator, times(1)).isOdd(51);
+        verify(calculator, never()).divide(anyDouble(), anyDouble());
+        verify(calculator, never()).multiply(anyDouble(), anyDouble());
+        verify(calculator, never()).add(anyDouble(), anyDouble());
+
+        final CalculationResult expectedCalculationResult = new CalculationResult(51, Color.RED);
+        assertEquals(expectedCalculationResult, actualCalculationResult);
     }
 }
